366d089aa12fd18fdd0c7db013d34b2db01f0304
Justin van Wees <justin@vwees.net>
2011-01-29 12:23:44 +0100

    Added basic intergration with Facebook (events are posted, updated and deleted to Facebook)

--- config/tournee_bootstrap.php
+++ config/tournee_bootstrap.php
@@ -7,4 +7,13 @@
 	#Croogo::hookAdminRowAction('Nodes/admin_index', 'Example', 'plugin:example/controller:example/action:index/:id');
 	#Croogo::hookAdminTab('Nodes/admin_add', 'Example', 'example.admin_tab_node');
 	#Croogo::hookAdminTab('Nodes/admin_edit', 'Example', 'example.admin_tab_node');
+	
+	# Load Tournee config
+	if(file_exists(APP.'plugins'.DS.'tournee'.DS.'config'.DS.'settings.yml')){
+		 $settings = Spyc::YAMLLoad(file_get_contents(APP.'plugins'.DS.'tournee'.DS.'config'.DS.'settings.yml'));
+	}
+	
+	foreach($settings AS $settingKey => $settingValue){
+		Configure::write($settingKey, $settingValue);
+	}
 ?>
--- controllers/tournee_events_controller.php
+++ controllers/tournee_events_controller.php
@@ -1,7 +1,8 @@
 <?php
+App::import('Lib', 'Tournee.FB');
 class TourneeEventsController extends TourneeAppController {
 	var $uses = array('Tournee.TourneeEvent');
-	
+
 	function admin_index(){
 		$this->set('title_for_layout', __('Events', true));
 		$events = $this->paginate('TourneeEvent');
@@ -15,14 +16,35 @@ class TourneeEventsController extends TourneeAppController {
 		if(!empty($this->data)){
 			$this->TourneeEvent->create();
 			if($this->TourneeEvent->save($this->data)){
-				$this->Session->setFlash(sprintf(__('Event saved!', true)));
-                $this->redirect(array('controller' => 'tournee_events', 'action' => 'index'));
+				if(Configure::read('Tournee.facebook_intergration') == 'enabled'){
+					$this->data['TourneeTour'] = $this->TourneeEvent->TourneeTour->findById($this->data['TourneeEvent']['tournee_tour_id']);
+					$this->data['TourneeLocation'] = $this->TourneeEvent->TourneeLocation->findById($this->data['TourneeEvent']['tournee_location_id']);
+					$fb_event = $this->__post_facebook_event($this->data);
+					if(array_key_exists('id', $fb_event)){
+						$this->TourneeEvent->read(null, $this->TourneeEvent->id);
+						$this->TourneeEvent->set(array('facebook_event_id' => $fb_event['id']));
+						$this->TourneeEvent->save();
+						$flash_message = __('Event saved and posted to Facebook!', true);
+					}
+					else {
+						$flash_message = __('Event saved, but there was a problem posting it to Facebook', true);
+					}
+				}
+				else {
+					$flash_message = __('Event saved!', true);
+				}
+				
+				$this->Session->setFlash($flash_message);
+				$this->redirect(array('controller' => 'tournee_events', 'action' => 'index'));
 			}
 			else {
 				$this->Session->setFlash(sprintf(__('The event could not be saved. Please, try again.', true)));
 			}
 		}
-		
+		if(Configure::read('Tournee.facebook_intergration') == 'enabled'){
+			$this->__connect_to_facebook();
+		}
+
 		$tours = $this->TourneeEvent->TourneeTour->find('list');
 		$locations = $this->TourneeEvent->TourneeLocation->find('list');
 		
@@ -46,7 +68,37 @@ class TourneeEventsController extends TourneeAppController {
 		if(!empty($this->data)){
 			$this->TourneeEvent->id = $id;
 			if($this->TourneeEvent->save($this->data)){
-				$this->Session->setFlash(sprintf(__('The event has been saved', true)));
+				if(Configure::read('Tournee.facebook_intergration') == 'enabled'){
+					$this->data['TourneeTour'] = $this->TourneeEvent->TourneeTour->findById($this->data['TourneeEvent']['tournee_tour_id']);
+					$this->data['TourneeLocation'] = $this->TourneeEvent->TourneeLocation->findById($this->data['TourneeEvent']['tournee_location_id']);
+					
+					if($event['TourneeEvent']['facebook_event_id']){
+						$fb_event = $this->__update_facebook_event($event['TourneeEvent']['facebook_event_id'], $this->data);
+						if($fb_event == 1){
+							$flash_message = __('Event updated and posted to Facebook!', true);
+						}
+						else {
+							$flash_message = __('Event updated, but there was a problem posting it to Facebook', true);
+						}
+					}
+					else {
+						$fb_event = $this->__post_facebook_event($this->data);
+						if(array_key_exists('id', $fb_event)){
+							$this->TourneeEvent->read(null, $this->TourneeEvent->id);
+							$this->TourneeEvent->set(array('facebook_event_id' => $fb_event['id']));
+							$this->TourneeEvent->save();
+							$flash_message = __('Event updated and posted to Facebook!', true);
+						}
+						else {
+							$flash_message = __('Event updated, but there was a problem posting it to Facebook', true);
+						}
+					}
+				}
+				else {
+					$flash_message = __('The event has been saved.', true);
+				}
+				
+				$this->Session->setFlash($flash_message);
                 $this->redirect(array('controller' => 'tournee_events', 'action' => 'index'));
 			}
 			else {
@@ -55,6 +107,10 @@ class TourneeEventsController extends TourneeAppController {
 		}
 		
 		$this->data = $event;
+		
+		if(Configure::read('Tournee.facebook_intergration') == 'enabled'){
+			$this->__connect_to_facebook();
+		}
 			
 		$tours = $this->TourneeEvent->TourneeTour->find('list');
 		$locations = $this->TourneeEvent->TourneeLocation->find('list');
@@ -68,13 +124,116 @@ class TourneeEventsController extends TourneeAppController {
 			$this->redirect(array('action' => 'index'));
 		}
 		
+		$event = $this->TourneeEvent->findById($id);
+		
 		if($this->TourneeEvent->delete($id)){
+			if(!empty($event['TourneeEvent']['facebook_event_id'])){
+				$this->__delete_facebook_event($event['TourneeEvent']['facebook_event_id']);
+			}
 			$this->Session->setFlash(__('Event deleted', true), 'default', array('class' => 'success'));
             $this->redirect(array('action' => 'index'));
 		}
 		else {
 			$this->Session->setFlash(__('Failed to delete the event', true), 'default', array('class' => 'error'));
             $this->redirect(array('action' => 'index'));
+		}
+	}
+	
+	function __connect_to_facebook(){
+		App::import('Vendor', 'Tournee.facebook/src/facebook');
+		$facebook = new Facebook(array(
+			'appId' => Configure::read('Tournee.facebook_app_id'),
+			'secret' => Configure::read('Tournee.facebook_app_secret'),
+			'cookie' => true
+		));
+		
+		$session = $facebook->getSession();
+		$login_url = $facebook->getLoginUrl(array(
+			'req_perms' => 'create_event, rsvp_event'
+		));
+		
+		if(!empty($session)){
+			$this->Session->write('uid', $session['uid']);
+		}
+		else {
+			$this->redirect($login_url);
+		}
+	}
+	
+	function __delete_facebook_event($facebook_event_id = null){
+		App::import('Vendor', 'Tournee.facebook/src/facebook');
+		$facebook = new Facebook(array(
+			'appId' => Configure::read('Tournee.facebook_app_id'),
+			'secret' => Configure::read('Tournee.facebook_app_secret'),
+			'cookie' => true
+		));
+
+		$session = $facebook->getSession();
+		if(!empty($session)){
+			return $facebook->api($facebook_event_id, 'DELETE');
+		}
+		else {
+			return false;
+		}
+	}
+	
+	function __update_facebook_event($facebook_event_id = null, $event_data = null){
+		App::import('Vendor', 'Tournee.facebook/src/facebook');
+		$facebook = new Facebook(array(
+			'appId' => Configure::read('Tournee.facebook_app_id'),
+			'secret' => Configure::read('Tournee.facebook_app_secret'),
+			'cookie' => true
+		));
+
+		$session = $facebook->getSession();
+
+		if(!empty($session)){
+			$this->Session->write('uid', $session['uid']);
+			$fb_event_array = array(
+				'name' => $event_data['TourneeTour']['TourneeTour']['title'],
+				'description' => $event_data['TourneeTour']['TourneeTour']['description'],
+				'start_time' => $event_data['TourneeEvent']['start_datetime']['year'].$event_data['TourneeEvent']['start_datetime']['month'].$event_data['TourneeEvent']['start_datetime']['day'].'T'.$event_data['TourneeEvent']['start_datetime']['hour'].$event_data['TourneeEvent']['start_datetime']['min'],
+				'location' => $event_data['TourneeLocation']['TourneeLocation']['name'],
+				'street' => $event_data['TourneeLocation']['TourneeLocation']['address_street'],
+				'city' => $event_data['TourneeLocation']['TourneeLocation']['address_city'],
+				'zip' => $event_data['TourneeLocation']['TourneeLocation']['address_zip'],
+				'country' => $event_data['TourneeLocation']['TourneeLocation']['address_country'],
+				'privacy' => 'OPEN'
+			);
+			return $facebook->api($facebook_event_id, 'POST', $fb_event_array);
+		}
+		else {
+			return false;
+		}
+	}
+	
+	function __post_facebook_event($event_data = null){
+		App::import('Vendor', 'Tournee.facebook/src/facebook');
+		$facebook = new Facebook(array(
+			'appId' => Configure::read('Tournee.facebook_app_id'),
+			'secret' => Configure::read('Tournee.facebook_app_secret'),
+			'cookie' => true
+		));
+
+		$session = $facebook->getSession();
+
+		if(!empty($session)){
+			$this->Session->write('uid', $session['uid']);
+			$fb_event_array = array(
+				'name' => $event_data['TourneeTour']['TourneeTour']['title'],
+				'description' => $event_data['TourneeTour']['TourneeTour']['description'],
+				'start_time' => $event_data['TourneeEvent']['start_datetime']['year'].$event_data['TourneeEvent']['start_datetime']['month'].$event_data['TourneeEvent']['start_datetime']['day'].'T'.$event_data['TourneeEvent']['start_datetime']['hour'].$event_data['TourneeEvent']['start_datetime']['min'],
+				'location' => $event_data['TourneeLocation']['TourneeLocation']['name'],
+				'street' => $event_data['TourneeLocation']['TourneeLocation']['address_street'],
+				'city' => $event_data['TourneeLocation']['TourneeLocation']['address_city'],
+				'zip' => $event_data['TourneeLocation']['TourneeLocation']['address_zip'],
+				'country' => $event_data['TourneeLocation']['TourneeLocation']['address_country'],
+				'privacy' => 'OPEN'
+			);
+			return $facebook->api('/me/events', 'POST', $fb_event_array);
+		}
+		else {
+			return false;
 		}
 	}
 }
